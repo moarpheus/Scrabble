51a10cfe175257758a9870b48a00d643af686189
Juris Gribuska <juris.gribuska@bt.com>
2021-05-19 09:09:28 +0100

    incorrect tests

--- scrabble_score.rb
+++ scrabble_score.rb
@@ -1,8 +1,26 @@
-=begin
-Write your code for the 'Scrabble Score' exercise in this file. Make the tests in
-`scrabble_score_test.rb` pass.
+require 'byebug'
 
-To get started with TDD, see the `README.md` file in your
-`ruby/scrabble-score` directory.
-=end
+class Scrabble
 
+  DEFAULTS = {
+    %w[A, E, I, O, U, L, N, R, S, T] => 1,
+    %w[D, G] => 2,
+    %w[B, C, M, P] => 3,
+    %w[F, H, V, W, Y] => 4,
+    %w[K] => 5,
+    %w[J, X] => 6,
+    %w[Q, Z] => 7
+  }
+
+  def initialize input
+    @@mappings = DEFAULTS.inject({}) do |memo, (key, value)|
+      key.each {|l| memo[l.gsub(',', '')] = value}
+      memo
+    end
+    input.nil? ? @input = '' : @input = input
+  end
+
+  def score input=@input
+    input.strip.upcase.split('').inject(0) {|sum,x| sum + @@mappings[x].to_i }
+  end
+end
